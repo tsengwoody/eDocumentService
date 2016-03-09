da36851997191fa5cd019983d1acc8c6991f79cb
方方 <t101598002@ntut.edu.tw>
2016-03-09 21:28:02 +0800

    1.add danger alert message 2.refactor next page and pre page

--- static/ebookSystem/js/edit.js
+++ static/ebookSystem/js/edit.js
@@ -1,105 +1,123 @@
-﻿function changePage(offset){
-	page=document.getElementById("id_page");
-	imgScanPage=$('#scanPage')[0];
-	src=imgScanPage.src;
-	src=src.split('/');
-	dirname=''
-	for (i = 0;i < src.length-1; i++) dirname=dirname+src[i]+'/';
-		filename=src[src.length-1];
-		extensionName=filename.split('.')[1];
-		filename=filename.split('.')[0];
-		scanPageList=document.getElementById("scanPageList");
-	if(scanPageList.selectedIndex+offset>=0&&scanPageList.selectedIndex+offset<scanPageList.length) {
-		scanPageList.selectedIndex=scanPageList.selectedIndex+offset;
-		page.value=scanPageList.selectedIndex;
-		imgScanPage.src=dirname+scanPageList.options[scanPageList.selectedIndex].value;
-	} else {
-		alert('超過頁數範圍惹~');
-	}
-}
-function changePageSelect() {
-	page=document.getElementById("id_page");
-	imgScanPage=$('#scanPage')[0];
-	src=imgScanPage.src;
-	console.log(src);
-	src=src.split('/');
-	dirname=''
-	for (i = 0;i < src.length-1; i++) 
-		dirname=dirname+src[i]+'/';
-	filename=src[src.length-1];
-	extensionName=filename.split('.')[1];
-	filename=filename.split('.')[0];
-	scanPageList=document.getElementById("scanPageList");
-	page.value=scanPageList.selectedIndex;
-	imgScanPage.src=dirname+scanPageList.options[scanPageList.selectedIndex].value;
-}
-function saveSubmit(event)
-{
-    if(typeof $('textarea').val()=='undefined')
-    {
-        event.preventDefault();
-        alert("not contain keyword");
-    }
-    var str=$('textarea').val();
-    if(str.indexOf("|----------|")<0)
-    {
-        event.preventDefault();
-        alert("not contain keyword");
-    }
-}
-function finishSubmit(event)
-{
-    if(typeof $('textarea').val()=='undefined')
-    {
-        event.preventDefault();
-        alert("not contain keyword");
-    }
-    var str=$('textarea').val();
-    if(str.indexOf("|----------|")>0)
-    {
-        event.preventDefault();
-        alert("can not contain keyword");
+﻿function changePage(offset) {
+    page = document.getElementById("id_page");
+    imgScanPage = $('#scanPage')[0];
+    src = imgScanPage.src;
+    src = src.split('/');
+    dirname = ''
+    for (i = 0; i < src.length - 1; i++) dirname = dirname + src[i] + '/';
+    filename = src[src.length - 1];
+    extensionName = filename.split('.')[1];
+    filename = filename.split('.')[0];
+    scanPageList = document.getElementById("scanPageList");
+    if (scanPageList.selectedIndex + offset >= 0 && scanPageList.selectedIndex + offset < scanPageList.length) {
+        scanPageList.selectedIndex = scanPageList.selectedIndex + offset;
+        page.value = scanPageList.selectedIndex;
+        imgScanPage.src = dirname + scanPageList.options[scanPageList.selectedIndex].value;
+    } else {
+        alert('超過頁數範圍惹~');
     }
 }
 
-$( document ).ready(function() {
-    console.log( "ready!" );
+function changePageSelect() {
+    page = document.getElementById("id_page");
+    imgScanPage = $('#scanPage')[0];
+    src = imgScanPage.src;
+    console.log(src);
+    src = src.split('/');
+    dirname = ''
+    for (i = 0; i < src.length - 1; i++)
+        dirname = dirname + src[i] + '/';
+    filename = src[src.length - 1];
+    extensionName = filename.split('.')[1];
+    filename = filename.split('.')[0];
+    scanPageList = document.getElementById("scanPageList");
+    page.value = scanPageList.selectedIndex;
+    imgScanPage.src = dirname + scanPageList.options[scanPageList.selectedIndex].value;
+}
+function dangerAlert(message)
+{
+    $("#alertMessage").html(message);
+    $("#danger-alert").alert();
+    $("#danger-alert").fadeTo(2000, 500).slideUp(500, function() {
+        $("#danger-alert").hide();
+    });
+}
+
+function saveSubmit(event) {
+    if (typeof $('textarea').val() == 'undefined') {
+        event.preventDefault();
+        dangerAlert("textarea not found")
+    }
+    var str = $('textarea').val();
+    if (str.indexOf("|----------|") < 0) {
+        event.preventDefault();
+        dangerAlert("not contain the keyword");
+    }
+}
+
+function finishSubmit(event) {
+    if (typeof $('textarea').val() == 'undefined') {
+        event.preventDefault();
+        dangerAlert("textarea not found");
+    }
+    var str = $('textarea').val();
+    if (str.indexOf("|----------|") > 0) {
+        event.preventDefault();
+        dangerAlert("can't contain the keyword");
+    }
+}
+
+$(document).ready(function() {
+    console.log("ready!");
+    $(".alert button.close").click(function (e) {
+        $(this).parent().fadeOut('slow');
+    });
+
+
+
+
+    $('#prePage').on("click", function() {
+        changePage(-1);
+    });
+    //nextPage
+    $('#nextPage').on("click", function() {
+        changePage(1);
+    });
+
     $('#save_id').click(saveSubmit);
     $('#finish_id').click(finishSubmit);
 
 
-    $('#zoomIN').click(function(){
-        imgSize.value=(parseInt(imgSize.value)-10).toString()+'%';
-        $('#scanPage').css('width',imgSize.value);
+    $('#zoomIN').click(function() {
+        imgSize.value = (parseInt(imgSize.value) - 10).toString() + '%';
+        $('#scanPage').css('width', imgSize.value);
 
     });
-    $('#zoomOUT').click(function(){
+    $('#zoomOUT').click(function() {
 
-        imgSize.value=(parseInt(imgSize.value)+10).toString()+'%';
-        $('#scanPage').css('width',imgSize.value);
+        imgSize.value = (parseInt(imgSize.value) + 10).toString() + '%';
+        $('#scanPage').css('width', imgSize.value);
 
     });
 
 
 
-    $('#chagePost').click(function(){
-    	if($('#imagePage').hasClass('col-md-6'))
-    	{
-    		$('#imagePage').removeClass("col-md-6");
-    		$('#imagePage').addClass("col-md-12");
-    	}else{
-    		$('#imagePage').removeClass("col-md-12");
-    		$('#imagePage').addClass("col-md-6");
-    	}
+    $('#chagePost').click(function() {
+        if ($('#imagePage').hasClass('col-md-6')) {
+            $('#imagePage').removeClass("col-md-6");
+            $('#imagePage').addClass("col-md-12");
+        } else {
+            $('#imagePage').removeClass("col-md-12");
+            $('#imagePage').addClass("col-md-6");
+        }
 
-    	if($('#textPage').hasClass('col-md-6'))
-    	{
-    		$('#textPage').removeClass("col-md-6");
-    		$('#textPage').addClass("col-md-12");
-    	}else{
-    		$('#textPage').removeClass("col-md-12");
-    		$('#textPage').addClass("col-md-6");
-    	}
+        if ($('#textPage').hasClass('col-md-6')) {
+            $('#textPage').removeClass("col-md-6");
+            $('#textPage').addClass("col-md-12");
+        } else {
+            $('#textPage').removeClass("col-md-12");
+            $('#textPage').addClass("col-md-6");
+        }
 
     });
 });
