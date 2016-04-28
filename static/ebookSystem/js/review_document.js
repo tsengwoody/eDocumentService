86c71527e3fed74c3c7d9dbf8fe22971cf8ad5a5
方方 <t101598002@ntut.edu.tw>
2016-04-28 19:44:46 +0800

    add js in review_document

--- static/ebookSystem/js/review_document.js
+++ static/ebookSystem/js/review_document.js
@@ -1,6 +1,43 @@
 function activaTab(tab){
     $('.nav-pills a[href="#' + tab + '"]').tab('show');
 };
+function changePage(offset) {
+    page = document.getElementById("id_page");
+    imgScanPage = $('#scanPage')[0];
+    src = imgScanPage.src;
+    src = src.split('/');
+    dirname = ''
+    for (i = 0; i < src.length - 1; i++) dirname = dirname + src[i] + '/';
+    filename = src[src.length - 1];
+    extensionName = filename.split('.')[1];
+    filename = filename.split('.')[0];
+    scanPageList = document.getElementById("scanPageList");
+    if (scanPageList.selectedIndex + offset >= 0 && scanPageList.selectedIndex + offset < scanPageList.length) {
+        scanPageList.selectedIndex = scanPageList.selectedIndex + offset;
+        page.value = scanPageList.selectedIndex;
+        imgScanPage.src = dirname + scanPageList.options[scanPageList.selectedIndex].value;
+    } else {
+        //dangerAlert('超過頁數範圍惹~');
+        alertMessageDialog('error','超過頁數範圍惹~');
+    }
+}
+
+function changePageSelect() {
+    page = document.getElementById("id_page");
+    imgScanPage = $('#scanPage')[0];
+    src = imgScanPage.src;
+    console.log(src);
+    src = src.split('/');
+    dirname = ''
+    for (i = 0; i < src.length - 1; i++)
+        dirname = dirname + src[i] + '/';
+    filename = src[src.length - 1];
+    extensionName = filename.split('.')[1];
+    filename = filename.split('.')[0];
+    scanPageList = document.getElementById("scanPageList");
+    page.value = scanPageList.selectedIndex;
+    imgScanPage.src = dirname + scanPageList.options[scanPageList.selectedIndex].value;
+}
 $(document).ready(function() {
     console.log("ready!");
     //activaTab('step2');
@@ -9,5 +46,13 @@ $(document).ready(function() {
     });
     $('#step2_next_id').click(function(){
         activaTab('step3');
+    });
+
+    $('#prePage').on("click", function() {
+        changePage(-1);
+    });
+    //nextPage
+    $('#nextPage').on("click", function() {
+        changePage(1);
     });
 });
