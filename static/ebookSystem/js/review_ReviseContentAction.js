f7b155b8c0466ffdbfaa01c081c55bddb64a8877
方方 <t101598002@ntut.edu.tw>
2016-07-10 12:39:27 +0800

    modify layout in review_ReviseContentAction.html

---
+++ static/ebookSystem/js/review_ReviseContentAction.js
@@ -0,0 +1,13 @@
+
+
+$( document ).ready(function() {
+    console.log( "document ready!" );
+    if($('.modal.fade.in').length>0)
+    {
+        $('.modal.fade.in').on('shown.bs.modal', function () {
+            $('.modal.fade.in .close').focus();
+        });
+        $('.modal.fade.in').modal();
+    }
+    $('[data-toggle="tooltip"]').tooltip();
+});
