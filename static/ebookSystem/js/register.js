2cc7d36c223e1438a1e9db284e59a3e93542bcba
方方 <t101598002@ntut.edu.tw>
2016-08-18 21:16:32 +0800

    adjust the layout

--- static/ebookSystem/js/register.js
+++ static/ebookSystem/js/register.js
@@ -16,12 +16,15 @@ function optradioChange()
 {
     $('.guest_mode').hide();
     $('.editor_mode').hide();
+    $('#id_service_guest').attr('readonly', true);
     $('input[type=checkbox]:checked').each(function(){
         console.log($(this).val());
         if($(this).val()=='Editor')
             $('.editor_mode').show();
         if($(this).val()=='Guest')
             $('.guest_mode').show();
+        if($(this).val()=='service_guest_check')
+            $('#id_service_guest').attr('readonly', false);
 
     });
 }
