beeb3ccf015b8a9d80ef6a549a339782a16c133e
tsengwoody <tsengwoody.tw@gmail.com>
2016-09-18 16:31:52 +0800

    初步建立好3種類型的特殊tag編輯頁

--- ebookSystem/urls.py
+++ ebookSystem/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+#	url(r'^advanced/edit_image/(?P<id>[0-9\-]+)/$', views.edit_image, name='edit_image'),
+#	url(r'^advanced/edit_mathml/(?P<id>[0-9\-]+)/$', views.edit_mathml, name='edit_mathml'),
+#	url(r'^advanced/edit_unknown/(?P<id>[0-9\-]+)/$', views.edit_unknown, name='edit_unknown'),
+	url(r'^advanced/edit_(?P<type>\w+)/(?P<id>[0-9\-]+)/$', views.edit_SpecialContent, name='edit_SpecialContent'),
 	url(r'^book_info/(?P<ISBN>[0-9Xx]+)/$', views.book_info, name='book_info'),
 	url(r'^book_list/$', views.book_list.as_view(), name='book_list'),
 	url(r'^special_content/(?P<ISBN_part>[\d-]+)/$', views.special_content, name='special_content'),
