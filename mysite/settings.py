5eb4308e0bd45729503054e42da68b21cf97ce5e
方方 <t101598002@ntut.edu.tw>
2016-11-08 16:05:19 +0800

    add seo

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-if DEBUG is False:
+if DEBUG is True:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
