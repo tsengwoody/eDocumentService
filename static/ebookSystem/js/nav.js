c20e77a75ab15cf1d32534ad1a74da55bcd6f8c1
root <root@localhost.localdomain>
2017-06-21 09:04:47 -0700

    modify remove ul

--- static/ebookSystem/js/nav.js
+++ static/ebookSystem/js/nav.js
@@ -1,9 +1,9 @@
 $( document ).ready(function() {
-  $('ul[class="dropdown-menu"]').find('li').each(function(){
-    let li=$(this);
-    let a=li.children();
-    if(a.attr('href')===''){
-        li.remove();
+  $('ul[class="dropdown-menu"]').each(function(){
+    let ul=$(this);
+    let cd=ul.children();
+    if(cd.length===0){
+        ul.parent().remove();
     }
   })
 });
