d89a7be046ed0565eb468f22a8cf34e618417243
root <root@localhost.localdomain>
2017-07-20 01:14:29 -0700

    1.更新背景ajax(非form)下載模板 2.更新tawk.to客服

---
+++ static/ebookSystem/js/ajaxDownloadFile.js
@@ -0,0 +1,17 @@
+function downloadfile(cfn, bindata) {
+	//下載binary資料成為檔案
+	//使用標籤a與blob物件直接將bindata下載成為檔案
+
+	//tag a
+	let downloadLink = document.createElement('a');
+	let blob = new Blob([bindata]);
+	let url = URL.createObjectURL(blob);
+	downloadLink.href = url;
+	downloadLink.download = cfn;
+
+	//download
+	document.body.appendChild(downloadLink);
+	downloadLink.click();
+	document.body.removeChild(downloadLink);
+
+}
