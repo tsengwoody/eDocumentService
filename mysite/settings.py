a3c1fd153638dc48ee549eee221f9819ee477814
tsengwoody <tsengwoody.tw@gmail.com>
2017-12-15 15:19:00 +0800

    change use mariadb to dev

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,8 +97,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-#if socket.gethostname() == 'edoc':
-if 1:
+if socket.gethostname() == 'edoc':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
@@ -112,10 +111,22 @@ if 1:
 else:
     DATABASES = {
         'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'eDocumentService',
+            'USER': 'root',
+            'PASSWORD': 'eds@2017',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+        }
+    }
+
+'''else:
+    DATABASES = {
+        'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-    }
+    }'''
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
