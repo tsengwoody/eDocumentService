c3c86591bfa51a15b0304287bba3ff6d4a622865
jamielin125 <zerobest125@hotmail.com.tw>
2018-07-12 04:55:41 -0700

    change style on disabilitycard register

--- templates/comp/drf.vue
+++ templates/comp/drf.vue
@@ -1,54 +1,54 @@
 <template id="drf-model">
-	<div class="form-horizontal">
-		<div class="form-group">
-			<label
-				:for="'id_' +keys"
-				class="col-sm-3"
-			>{|{ convertTitle[keys] }|}</label>
-			<template v-if="mode==='read'">
-				<div
-					v-if="model_info.type==='choice' || model_info.type==='field'"
-					class="col-sm-9"
-				>{|{ value2display_name(value, model_info.choices) }|}</div>
-				<div
-					v-else
-					class="col-sm-9"
-				>{|{ value }|}</div>
-			</template>
-			<template v-if="mode==='write'">
-				<input
-					v-if="model_info.type==='string'"
-					v-bind:value="value"
-					v-on:input="$emit('input', $event.target.value)"
-					v-bind:id="'id_' +keys"
-					class="col-sm-9 form-control"
+	<div class="form-group">
+		<label
+			:for="'id_' +keys"
+			class="col-sm-3"
+		>{|{ convertTitle[keys] }|}</label>
+		<template v-if="mode==='read'">
+			<div
+				v-if="model_info.type==='choice' || model_info.type==='field'"
+				class="col-sm-9"
+			>{|{ value2display_name(value, model_info.choices) }|}</div>
+			<div
+				v-else
+				class="col-sm-9"
+			>{|{ value }|}</div>
+		</template>
+		<template v-if="mode==='write'">
+		<div class="col-sm-9">
+			<input
+				v-if="model_info.type==='string'"
+				v-bind:value="value"
+				v-on:input="$emit('input', $event.target.value)"
+				v-bind:id="'id_' +keys"
+				class="form-control"
+			>
+			<select
+				v-if="model_info.type==='choice' || model_info.type==='field'"
+				v-bind:value="value"
+				v-on:input="$emit('input', $event.target.value)"
+				v-bind:id="'id_' +keys"
+				class="form-control"
+			>
+				<option
+					v-for="item in model_info.choices"
+					:value="item.value"
 				>
-				<select
-					v-if="model_info.type==='choice' || model_info.type==='field'"
-					v-bind:value="value"
-					v-on:input="$emit('input', $event.target.value)"
-					v-bind:id="'id_' +keys"
-					class="col-sm-9 form-control"
-				>
-					<option
-						v-for="item in model_info.choices"
-						:value="item.value"
-					>
-						{|{ item.display_name }|}
-					</option>
-				</select>
-				<el-date-picker
-					v-if="model_info.type==='date'"
-					v-bind:value="value"
-					v-on:input="$emit('input', $event)"
-					v-bind:id="'id_' +keys"
-					value-format="yyyy-MM-dd"   
-					placeholder="yyyy-MM-dd"
-					size=small
-					style="width: 100%;"
-				></el-date-picker>
-			</template>
+					{|{ item.display_name }|}
+				</option>
+			</select>
+			<el-date-picker
+				v-if="model_info.type==='date'"
+				v-bind:value="value"
+				v-on:input="$emit('input', $event)"
+				v-bind:id="'id_' +keys"
+				value-format="yyyy-MM-dd"   
+				placeholder="yyyy-MM-dd"
+				size=small
+				style="width: 100%;"
+			></el-date-picker>
 		</div>
+		</template>
 	</div>
 </template>
 <script>
