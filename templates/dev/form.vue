024685792555076f521eeb07c1f765640748465a
jamielin125 <zerobest125@hotmail.com.tw>
2018-07-31 04:35:32 -0700

    add vue vregister & fix userr_person style

--- templates/dev/form.vue
+++ templates/dev/form.vue
@@ -1,57 +1,68 @@
 ﻿<template id="bs-form-group">
-	<div class="form-horizontal">
-		<template v-for="item in fields">
-			<div class="form-group">
-				<label class="control-label col-sm-2" :for="'id_' +item.keys"><font style="color:red">*</font><span>{|{ item.model_info.label }|}</span></label>
-				<div class="col-sm-4">
-					<input
-						v-if="item.model_info.type === 'text' || item.model_info.type === 'email' || item.model_info.type === 'checkbox'"
-						class="form-control" 
-						:type="item.model_info.type" 
-						:id="'id_' +item.keys"
-						v-model="new_datas[item.keys]"
-					>
+	<div class="form-group">
+		<label class="control-label col-sm-2 col-sm-offset-2" :for="field"><font style="color:red">*</font><span>{|{ model_info.label }|}</span></label>
+		<div class="col-sm-3">
+			<input
+				v-if="model_info.type === 'text' || model_info.type === 'email' || model_info.type === 'password'"
+				class="form-control" 
+				:type="model_info.type" 
+				:id="field"
+				:value="value"
+				@input="$emit('input', $event.target.value)"
+			>
 
-					<el-date-picker v-if="item.model_info.type === 'date'"
-						:id="'id_' +item.keys"
-						v-model="new_datas[item.keys]"
-						value-format="yyyy-MM-dd"   
-						placeholder="yyyy-MM-dd"
-						size=small
-						style="width: 100%;"
-					></el-date-picker>
+			<input
+				v-if="model_info.type === 'checkbox'"
+				:type="model_info.type" 
+				:id="field"
+				:value="value"
+				@change="$emit('input', $event.target.checked)"
+				:checked="value == true"
+				style="height: 30px; line-height: 30px;"
+			>
 
-					<select v-if="item.model_info.type === 'select'"
-						class="form-control"
-						:id="'id_' +item.keys"
-						v-model="new_datas[item.keys]"
-					>
-						<option
-							v-for="option_item in item.model_info.choices"
-							:value="option_item.value"
-						>
-							{|{ option_item.display_name }|}
-						</option>
-					</select>
+			<el-date-picker v-if="model_info.type === 'date'"
+				:id="field"
+				:value="value"
+				@input="$emit('input', $event)"
+				value-format="yyyy-MM-dd"   
+				placeholder="yyyy-MM-dd"
+				size=small
+				style="width: 100%;"
+			></el-date-picker>
 
-					<template
-						v-if="item.model_info.type === 'radio'" 
-						v-for="option_item in item.model_info.choices"
-					>
-						<label class="radio-inline">
-						<input 
-							type="radio" 
-							:id="'id_' +item.keys"
-							:value="option_item.value"
-						v-model="new_datas[item.keys]"
-						> 
-							{|{ option_item.display_name }|}
-						</label>
-					</template>
-				</div>
-				<label class="control-label col-sm-4" :for="'id_' +item.keys" style="text-align:left;"><font style="color:red">{|{ item.model_info.remark }|}</font></label>
-			</div>
-		</template>
+			<select v-if="model_info.type === 'select'"
+				class="form-control"
+				:id="field"
+				:value="value"
+				@input="$emit('input', $event.target.value)"
+			>
+				<option
+					v-for="(name, val) in model_info.choices"
+					:value="val"
+				>
+					{|{ name }|}
+				</option>
+			</select>
+
+			<template
+				v-if="model_info.type === 'radio'" 
+				v-for="(name, val) in model_info.choices"
+			>
+				
+				<label class="radio-inline">
+				<input 
+					type="radio" 
+					:id="val"
+					:value="val"
+					@input="$emit('input', $event.target.value)"
+					:checked="val == value"
+				> 
+					{|{ name }|}
+				</label>
+			</template>
+		</div>
+		<label class="control-label col-sm-4" :for="field" style="text-align:left;"><font style="color:red">{|{ model_info.remark }|}</font></label>
 	</div>
 </template>
 
@@ -60,28 +71,8 @@
 
 	Vue.component('form-drf', {
 		template: '#bs-form-group',
-		props: ['fields', 'url', 'bus'],
-		data: function(){
-			return {
-				new_datas: {},
-				origin_datas: {},
-			}
-		},
+		props: ['model_info', 'field', 'value'],
 		methods: {
-			get: function() {
-				this.$emit('get', this.new_datas)
-			},
-		},
-		created: function() {
-			let self = this;
-
-			_.each(self.fields, function(v){
-				self.origin_datas[v.keys] = v.value
-			});
-			self.new_datas = _.cloneDeep(self.origin_datas); // back up
-		},
-		mounted () {
-			this.bus.$on('get', this.get);
 		},
 	});
 
