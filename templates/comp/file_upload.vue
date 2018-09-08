18422e89dcc97f5684f1c0914f305400257cdedf
root <root@localhost.localdomain>
2018-09-07 23:19:05 -0700

    change style & check file extension

--- templates/comp/file_upload.vue
+++ templates/comp/file_upload.vue
@@ -5,45 +5,66 @@
 			<h4 class="modal-title">檔案上傳</h4>
 		</template>
 		<template slot="body">
-			<div v-if="!(mode==='delete')" class="form-group">
-				<div>
-					<label for="id_resource_name" class="control-label col-sm-1">檔案名稱：</label>
-					<div v-if="mode==='create'" class="col-sm-11">
-						<input
-							id="id_resource_name"
-							v-model="resource_name"
-							type="text"
-						>
+			<div class="form-horizontal">
+				<div v-if="!(mode==='delete')">
+					<div class="form-group">
+						<label for="id_resource_name" class="control-label col-sm-3">檔案名稱：</label>
+						<div v-if="mode==='create'" class="col-sm-3">
+							<input
+								id="id_resource_name"
+								class="form-control"
+								v-model="resource_name"
+								type="text"
+							>
+						</div>
+						<div v-if="mode==='update'" class="col-sm-3 likebtn">
+							{|{ resource_name }|}
+						</div>
 					</div>
-					<div v-if="mode==='update'" class="col-sm-11">
-						{|{ resource_name }|}
+					<div class="form-group">
+						<label class="control-label col-sm-3">檔案選擇：</label>
+						<div class="col-sm-6">
+							<input
+								id="id_resource"
+								type="file"
+							>
+						</div>
 					</div>
-
-					<label class="control-label col-sm-1">檔案選擇：</label>
-					<div class="col-sm-11">
-						<input
-							id="id_resource"
-							type="file"
-						>
-					</div>
-					<button
-						@click="upload_resource()"
-						class="btn btn-primary"
-					>上傳</button>
+				</div>
+				<div v-else>
+					<h3>確定要刪除文件？</h3>
 				</div>
 			</div>
-			<div v-else>
-				<p>確定要刪除文件？</p>
-				<button
-					class="btn btn-default"
-					@click="delete_resource()"
-				>刪除</button>
-			</div>
+		</template>
+		<template slot="footer">
+			<button v-if="!(mode==='delete')"
+				ref="up"
+				@click="upload_resource();"
+				class="btn btn-default"
+			>上傳</button>
+
+			<button v-else
+				ref="del"
+				class="btn btn-default"
+				@click="delete_resource();"
+			>刪除</button>
+
+			<button
+				onclick="closeDialog(this)"
+				class="btn btn-default"
+			>關閉</button>
 		</template>
 	</modal>
 </template>
 <script>
 	Vue.options.delimiters = ['{|{', '}|}'];
+
+	function getExt(filename){
+		// 依照名稱找副檔名
+		var ext = filename.split('.').pop();
+	    if(ext == filename) return '';
+	    return ext;
+	}	
 
 	Vue.component('file_upload', {
 		template: '#file_upload',
@@ -68,18 +89,43 @@
 				this.mode = mode
 			},
 			upload_resource: function(){
-				let self = this
-
+				let self = this;
 				let fileresource = document.getElementById('id_resource');
-				fileresourceObject = fileresource.files[0]
-				if(iser(fileresourceObject)){
+				let fileresourceObject = fileresource.files[0];
+				let resource_name = self.resource_name;
+
+				let fileExtension = '';
+				if(!iser(fileresourceObject)){
+					fileExtension = getExt(fileresourceObject.name);
+				} else {
 					alertmessage('error', '尚未選擇上傳附件')
+					return false;
 				}
+
+				let nameExtension = '';
+				if(resource_name) {
+					nameExtension = getExt(resource_name);
+				} else {
+					alertmessage('error', '輸入檔案名稱')
+					return false;
+				}
+
+				if(fileExtension && nameExtension) {
+					if(fileExtension !== nameExtension) {
+						self.resource_name = resource_name + '.' + fileExtension;
+						// alertmessage('question', '檔案附檔名不一致')
+					}
+				} else if (fileExtension && !nameExtension) {
+					self.resource_name = resource_name + '.' + fileExtension;
+				} 
 
 				rest_aj_upload(self.url +self.resource_name +'/', {'object': fileresourceObject})
 				.done(function(data) {
 					self.$emit('change')
 					alertmessage('success', '成功上傳檔案')
+					.done(function(){
+						closeDialog(self.$refs.up);
+					})
 				})
 				.fail(function(xhr, result, statusText){
 					console.log(xhr)
@@ -92,8 +138,11 @@
 				rest_aj_send('delete', self.url +self.resource_name +'/', {})
 				.done(function(data) {
 					self.$emit('change')
-					self.change_mode('create')
+					// self.change_mode('create')
 					alertmessage('success', '成功刪除檔案')
+					.done(function(){
+						closeDialog(self.$refs.del);
+					})
 				})
 				.fail(function(xhr, result, statusText){
 					console.log(xhr)
