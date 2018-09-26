60210f30082797e26c845c5e567e7ca111d74181
tsengwoody <tsengwoody.tw@gmail.com>
2018-09-26 01:42:01 -0700

    fix login bug

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
     'ebookSystem',
     'genericUser',
     'rest_framework',
