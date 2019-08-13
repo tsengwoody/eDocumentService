87bc485e19cc0a4b7d3f70b20985c5ae6407adb8
jamielin125 <jamielin125@alum.ccu.edu.tw>
2019-08-13 23:19:51 +0800

    update school home page

--- static/ebookSystem/js/vue-component/home_self.vue
+++ static/ebookSystem/js/vue-component/home_self.vue
@@ -1,117 +1,122 @@
 ﻿<template>
 	<div>
 		<div class="homeCarousel">
-			<h2 class="sr-only">首頁</h2>
-			<template v-for="(item, index) in bannercontentlist">
-				<div class="slides active" v-if="index==0">
-					<a :href="item.url">
-						<img
-							:src="`/genericUser/api/bannercontents/` +item.id +`/resource/cover/image`"
-							:alt="item.title"
-							:title="item.title"
-							style="height: 480px;width:940px" 
-						>
+			<h2 class="sr-only">校園版首頁</h2>
+			<div class="school-slide">
+				<img
+					src="static/ebookSystem/img/school/banner.png"
+					alt=""
+					title=""
+					style="width: 100%" 
+				>
+				<div class="slide-icons">
+					<a href="https://line.me/R/ti/g/eYf_3Ast89" target="_blank">
+						<img src="static/ebookSystem/img/school/line.png" alt="link to Line account">
+					</a>
+					<a href="https://www.facebook.com/forblind/" target="_blank">
+						<img src="static/ebookSystem/img/school/fb-icon.png" alt="link to Facebook page">
+					</a>
+					<a href="https://www.youtube.com/channel/UC8TV4EKxbSQPzx8nmTzfIag" target="_blank">
+						<img src="static/ebookSystem/img/school/youtube.png" alt="link to Youtube page">
 					</a>
 				</div>
-				<div class="slides" v-if="index!=0">
-					<a :href="item.url">
-						<img
-							:src="`/genericUser/api/bannercontents/` +item.id +`/resource/cover/image`"
-							:alt="item.title"
-							:title="item.title"
-							style="height: 480px;width:940px" 
-						>
-					</a>
+			</div>
+		</div>
+
+		<form class="form-inline form-row">
+			<div class="form-group">
+				列表排序：依
+				<label>
+					<input type="radio" name="sortField"> 發佈日期
+				</label>
+
+				<label>
+					<input type="radio" name="sortField"> 點閱次數
+				</label>
+
+				<button class="btn btn-default u-margin-left-sm">排序</button>
+			</div>
+		</form>
+
+		<form class="form-inline form-row">
+			<div class="form-group">
+				
+				<div class="form-group">
+					<label for="sel1">發佈單位：依</label>
+					<select class="form-control">
+						<option>org 1</option>
+						<option>org 2</option>
+						<option>org 3</option>
+						<option>org 4</option>
+					</select>
 				</div>
-			</template>
+
+				<label class="u-margin-left-sm">
+					關鍵字：<input type="text" class="form-control">
+				</label>
+
+				<button class="btn btn-default u-margin-left-sm">搜尋</button>
+			</div>
+		</form>
+
+		<h3>校園公佈欄列表</h3>
+		<div class="row">
 			
-			<a role="button" class="left carousel-control" @click="plusDivs(-1)">
-				<span class="pointer-left" aria-hidden="true">&#10094;</span>
-				<span class="sr-only">上一頁</span>
-			</a>
-			<a role="button" class="right carousel-control" @click="plusDivs(1)">
-				<span class="pointer-right" aria-hidden="true">&#10095;</span>
-				<span class="sr-only">下一頁</span>
-			</a>
-
-			<div class="textfornvda">
-				<h3>輪播圖片說明</h3>
-				<ul>
-					<li v-for="(item, index) in bannercontentlist">
-						<div v-html="markdown2html(item.content)"></div>
-					</li>
-				</ul>
+			<div style="float: left;width: 65%;margin: 0px 1rem;">
+				<table-div :datas="datas" :header="header">
+				</table-div>
 			</div>
-		</div>
+			
 
-		<div class="row introduction-area">
-			<button type="button" class="btn btn-secondary btn-lg">校園版服務介紹</button>
-			<button type="button" class="btn btn-secondary btn-lg" @click="window.location.replace('/routing/genericUser/announcement_school_list/')">平台系統公告</button>
-			<button type="button" class="btn btn-secondary btn-lg">協會簡介</button>
-			<button type="button" class="btn btn-secondary btn-lg" @click="window.location.replace('/routing/about/copyright/')">著作權說明</button>
-		</div>
-
-		<div class="row" style="padding-top: 20px;">
-			<div class="col-lg-4">
-				<div class="panel panel-success">
-					<div class="panel-heading">校園管理</div>
-					<div class="panel-body">
-						<ul class="navigation">
-							<li><a class="btn btn-link" 
-								tabindex="0"
-								@click="window.location.replace('/routing/ebookSystem/book_manager/')"
-							>校對進度查詢</a></li>
-							<li><a class="btn btn-link" 
-								tabindex="0"
-								@click="window.location.replace('/routing/genericUser/user_manager/')"
-							>志工時數查詢</a></li>
-						</ul> 
-					</div>
+			<div style="width: 30%; float: left;">
+				<div class="col-sm-4">
+					<a href="#" class="tool-box">
+						<div class="tool-box__img">
+							<img src="static/ebookSystem/img/school/sun.png" alt="" style="height:100%;">
+						</div>
+						<div class="tool-box__text">志工招募</div>
+					</a>
 				</div>
-			</div>
-			<div class="col-lg-4">
-				<div class="panel panel-success" itemscope="" itemtype="http://schema.org/Product">
-					<div class="panel-heading" itemprop="name">服務使用者</div>
-					<div class="panel-body" itemprop="description">
-						<ul class="navigation">
-							<li><a class="btn btn-link" 
-								tabindex="0"
-								@click="window.location.replace('/routing/ebookSystem/book_repository_school/')"
-							>書籍快速下載</a></li>
-							<li><a class="btn btn-link" 
-								href="#"
-							>應用工具下載</a></li>
-						</ul>
-					</div>
+			
+				<div class="col-sm-4">
+					<a href="#" class="tool-box">
+						<div class="tool-box__img">
+							<img src="static/ebookSystem/img/school/wifi.png" alt="" style="height:100%;">
+						</div>
+						<div class="tool-box__text">關於我們</div>
+					</a>
 				</div>
-			</div>
-			<div class="col-lg-4">
-				<div class="panel panel-success" itemscope="" itemtype="http://schema.org/Product">
-					<div class="panel-heading" itemprop="name">志工專區</div>
-					<div class="panel-body" itemprop="description">
-						<ul class="navigation">
-							<li><a class="btn btn-link" 
-								tabindex="0"
-								@click="window.location.replace('/routing/ebookSystem/service/')"
-							>快速領書</a></li>
-							<li><a class="btn btn-link" 
-								tabindex="0"
-								@click="window.location.replace('/routing/about/proofread_rules/')"
-							>校對規則說明</a></li>
-							<li><a class="btn btn-link" 
-								tabindex="0"
-								@click="window.location.replace('/routing/about/service_hours_description/')"
-							>服務時數說明</a></li>
-							<li><a class="btn btn-link" 
-								tabindex="0"
-								@click="window.location.replace('/routing/genericUser/user_person/')"
-							>帳號管理</a></li>
-							<li><a class="btn btn-link" 
-								tabindex="0"
-								@click="window.location.replace('/routing/genericUser/serviceinfo_record/')"
-							>服務時數</a></li>
-						</ul>
-					</div>
+				<div class="col-sm-4">
+					<a href="#" class="tool-box">
+						<div class="tool-box__img">
+							<img src="static/ebookSystem/img/school/pen.png" alt="" style="height:100%;">
+						</div>
+						<div class="tool-box__text">聯絡我們</div>
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a href="#" class="tool-box">
+						<div class="tool-box__img">
+							<img src="static/ebookSystem/img/school/tool.png" alt="" style="height:100%;">
+						</div>
+						<div class="tool-box__text">校園管理</div>
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a href="#" class="tool-box">
+						<div class="tool-box__img">
+							<img src="static/ebookSystem/img/school/boy.png" alt="" style="height:100%;">
+						</div>
+						<div class="tool-box__text">讀者專區</div>
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<a href="#" class="tool-box">
+						<div class="tool-box__img">
+							<img src="static/ebookSystem/img/school/bird.png" alt="" style="height:100%;">
+						</div>
+						<div class="tool-box__text">捐書捐款</div>
+					</a>
 				</div>
 			</div>
 		</div>
@@ -120,161 +125,100 @@
 
 <script>
 
-	function Timer(fn, t) {
-	    var timerObj = setInterval(fn, t);
-
-	    this.stop = function() {
-	        if (timerObj) {
-	            clearInterval(timerObj);
-	            timerObj = null;
-	        }
-	        return this;
-	    }
-
-	    // start timer using current settings (if it's not already running)
-	    this.start = function() {
-	        if (!timerObj) {
-	            this.stop();
-	            timerObj = setInterval(fn, t);
-	        }
-	        return this;
-	    }
-
-	    // start with new interval, stop current interval
-	    this.reset = function(newT) {
-	        t = newT;
-	        return this.stop().start();
-	    }
-	}
-
 	module.exports = {
+		components: {
+			'table-div': components['table-div-order'],
+		},
 		data: function() {
 			return {
-				bannercontentlist: null,
-				announcementlist: null,
-				slideIndex: 1,
-				timer: null,
+				header: {
+					title: '標題',
+					publishDate: '發佈日期',
+					org: '發佈單位',
+					readCount: '點閱次數',
+				},
+				datas: [
+					{
+						title: 'aaa',
+						publishDate: '2019/06/24',
+						org: 'org 1',
+						readCount: '12',
+					},
+					{
+						title: 'bbb',
+						publishDate: '2019/06/24',
+						org: 'org 2',
+						readCount: '13',
+					},
+				]
 			}
 		},
 		computed: {
-			showAnnouncements(){
-				let target_announcements = [];
-				if (this.announcementlist) {
-					if(this.announcementlist.length < 3) {
-						target_announcements = this.announcementlist;
-					} else {
-						target_announcements = this.announcementlist.slice(0, 2);
-					}
-				}
-				return target_announcements.map(function(announcement) {
-					let year, month, day;
-					[year, month, day] = announcement.datetime.split('-');
-					return {
-						id: announcement.id,
-						title: announcement.title,
-						year,
-						month,
-						day,
-					}
-				});
-			}
 		},
 		mounted(){
-			Promise.all([
-				genericUserAPI.bannerContentRest.filter({category: 'self'}),
-				genericUserAPI.announcementRest.list(),
-			])
-			.then(res => {
-				this.bannercontentlist = res[0].data;
-				this.announcementlist = res[1].data;
-
-				setTimeout(() => {
-					this.showDivs(this.slideIndex);
-				}, 100);
-
-				this.timer = new Timer(() => {
-					this.plusDivs(1);
-				}, 4000);
-
-			})
-
 		},
 		methods: {
-			markdown2html(text){
-				const converter = new showdown.Converter();
-				const html = converter.makeHtml(text);
-				return html;
-			},
-			plusDivs(n) {
-			  	this.showDivs(
-			  		this.slideIndex += n
-			  	);
-			  	this.timer.reset(4000);
-			},
-			showDivs(n) {
-				const slides = document.getElementsByClassName("slides");
-				if (n > slides.length) {
-					this.slideIndex = 1
-				}
-				if (n < 1) {
-					this.slideIndex = slides.length
-				}
-				for (let i = 0; i < slides.length; i++) {
-					slides[i].style.display = "none";  
-				}
-
-				if (slides.length > 0) {
-					slides[ this.slideIndex - 1 ].style.display = "block";
-				}
-			}
 		},
 	}
 </script>
 
 <style>
-.carousel-inner > .item > img,
-.carousel-inner > .item > a > img {
-  margin: auto;
+
+a:hover {
+	color:inherit;
+	text-decoration: none;
 }
 
-.homeCarousel {
-	position: relative;
-	width: 100%;
-	overflow: hidden;
+.u-margin-left-sm {
+	margin-left: 1rem;
 }
 
-@keyframes opac{
-	from{opacity:0} 
-	to{opacity:1}
+.tool-box {
+	display: inline-block;    
+	width: 100px;
+    height: 100px; 
+    border: 2px solid black;     
+    margin: 0 1.2rem 1.2rem 0;
+ 	text-align: center;
+ 	border-radius: 3px;
 }
 
-.slides {
-	display:none; 
-	animation: opac 0.8s;
+.tool-box__img {
+	height: 70px;
+    width: 100%;
+    margin-top: 0.2rem;
+    margin-bottom: 0.3rem;
 }
 
-.slides img {
-	margin: auto;
+.tool-box__text {
+	margin-top: 0.2rem;
+    color: red;
+    font-weight: 600;
+    letter-spacing: 2px;
+}
+
+.school-slide {
 	display: block;
-    max-width: 100%;
+    position: relative;
 }
 
-.pointer-left, .pointer-right {
+.slide-icons {
 	position: absolute;
-    top: 50%;
-    z-index: 5;
-    display: inline-block;
-    font-size: 1.2em;
+    right: 1rem;
+    bottom: 0.5rem;
 }
 
-.pointer-right {
-	right: 50%;
-    margin-right: -10px;
+.slide-icons img {
+	height: 3rem;
+	margin-left: 1rem;
 }
 
-.pointer-left {
-	left: 50%;
-    margin-left: -10px;
+label {
+    font-weight: 500;
+}
+
+.form-row {
+	margin: 1rem;
 }
 
 ul.navigation {
